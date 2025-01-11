9e5fe0c269fef77a4b0fdc329fb793a902017a74
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-01-11 21:11:03 +0530

    Initilized a boilerplate code

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get('/signup',function(req, res){
+
+});
+
+app.get('/singin',function(req, res){
+
+});
+
+app.get('/create-room',function(req, res){
+
+});
+
+app.listen(3001);
