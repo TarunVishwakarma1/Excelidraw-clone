092bfccac0086ef9c46ffde543b543ecf88f495e
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2025-01-18 14:15:23 +0530

    added some database connectivity to signup endpoint in http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,19 +3,26 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
 
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
         res.json({
             message: "Incorrect inputs"
         })
         return;
     }
-    // db call
+    prismaClient.user.create({
+        data:{
+            email:parsedData.data.username,
+            password:parsedData.data.password,
+            name:parsedData.data.name
+        }
+    });
     res.json({
         userId: "123"
     })
